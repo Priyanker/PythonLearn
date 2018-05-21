d14758688201a5503df1992a2897d5e52844b7a4
Priyanker <priyankerrao@gmail.com>
2018-05-21 18:28:52 +0530

    Adding database programs 1st time

---
+++ Databases/insertion.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+conn = sqlite3.connect('music.sqlite')
+cur = conn.cursor()
+
+
+cur.execute('DROP TABLE IF EXISTS Tracks')
+cur.execute('CREATE TABLE Tracks (title TEXT, plays INTEGER)')
+
+
+cur.execute("INSERT INTO Tracks (title, plays) VALUES (?,?)",('One', 12))
+cur.execute("INSERT INTO Tracks (title, plays) VALUES (?,?)",('Roots', 21))
+conn.commit()
+
+
+print("Tracks:")
+cur.execute('SELECT title, plays FROM Tracks')
+for row in cur:
+    print(row)
+print("deleting rows which have plays more than 20")
+cur.execute('DELETE FROM Tracks WHERE plays > 20')
+print("after deletion")
+cur.execute('SELECT title, plays FROM Tracks')
+for row, k in cur:
+    print(row, k)
+conn.close()
