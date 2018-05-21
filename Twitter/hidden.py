7909439aaf28feaea4d2c44a399f4bb7ab4263b5
Priyanker <priyankerrao@gmail.com>
2018-05-21 15:55:01 +0530

    first commit

---
+++ Twitter/hidden.py
@@ -0,0 +1,10 @@
+# Keep this file separate
+
+# https://apps.twitter.com/
+# Create new App and get the four strings
+
+def oauth():
+    return {"consumer_key": "KN20wdGusIVzb5DO1DGa7r7m0",
+            "consumer_secret": "zRmuzHO4ExCMnrZVTHk3IouVWcr5RNgLYcgNt6gAdluIO2212R",
+            "token_key": "3109128018-HE8cRGRqoLvRDZuDL6vDQlS09zLjwnBYbsyyowf",
+            "token_secret": "Q5LFrSeO3ZulfIb4lITfTvPE93ZfUrlG0CqQGdmDxavqZ"}
