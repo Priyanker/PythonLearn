3fbdd551110554216b7823af85c1e178be8b8fea
Priyanker <priyankerrao@gmail.com>
2018-05-23 13:49:02 +0530

    Adding practice problems 1st commit

---
+++ Practice Problems/wordcount.py
@@ -0,0 +1,27 @@
+d = dict()
+print("AS")
+fname = input("Enter file name(without extenstion:)")
+fh = open(fname + ".txt")
+for line in fh:
+    line = line.rstrip()
+    if len(line) == 0:
+        continue
+    words = line.split()
+    if words[0] == "From":
+        time = words[5].split(":")
+        d[time[0]] = d.get(time[0], 0) + 1
+l = list()
+for k, v in d.items():
+    l.append((k, v))
+
+l.sort()
+
+for (k, v) in l:
+    print(k, v)
+
+#l = list()
+# for k, v in d.items():
+#    l.append((v,k))
+
+# l.sort(reverse=True)
+#print("%s %d" % (l[0][1], l[0][0]))
