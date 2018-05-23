45a97270378fb06e2de598c3a0e0f846181a780e
Priyanker <priyankerrao@gmail.com>
2018-05-23 21:16:27 +0530

    Autoformatting in Atom for (insertion.py)

--- Databases/insertion.py
+++ Databases/insertion.py
@@ -8,9 +8,9 @@ cur.execute('DROP TABLE IF EXISTS Tracks')
 cur.execute('CREATE TABLE Tracks (title TEXT, plays INTEGER)')
 
 
-cur.execute("INSERT INTO Tracks (title, plays) VALUES (?,?)",('One', 12))
-cur.execute("INSERT INTO Tracks (title, plays) VALUES (?,?)",('Roots', 21))
-#conn.commit()
+cur.execute("INSERT INTO Tracks (title, plays) VALUES (?,?)", ('One', 12))
+cur.execute("INSERT INTO Tracks (title, plays) VALUES (?,?)", ('Roots', 21))
+# conn.commit()
 
 
 print("Tracks:")
@@ -19,7 +19,7 @@ for row in cur:
     print(row)
 print("deleting rows which have plays more than 20")
 cur.execute('DELETE FROM Tracks WHERE plays > 20')
-#conn.commit()
+# conn.commit()
 print("after deletion")
 cur.execute('SELECT title, plays FROM Tracks')
 for row, k in cur:
