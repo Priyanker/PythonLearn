daa1658f35d593f220996388aeb17f54bd76fbfe
Priyanker <priyankerrao@gmail.com>
2018-05-26 16:00:31 +0530

    added DrawnWrite program

---
+++ OpenCV/OpenCVTut/DrawnWrite.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+img = cv2.imread('watch.jpg', cv2.IMREAD_COLOR)
+
+cv2.line(img, (10, 10), (800, 800), (0, 255, 0), 15)
+cv2.rectangle(img, (15, 25), (200, 150), (255, 0, 0), 5)
+cv2.circle(img, ((200+15)//2, (150+25)//2), (150-25)//2, (0, 0, 255), -1)
+pts = np.array([[10, 5], [20, 30], [70, 20], [50, 10]], np.int32)
+# OpenCV documentation had this code, which reshapes the array to a 1 x 2. I did not
+# find this necessary, but you may:
+#pts = pts.reshape((-1,1,2))
+cv2.polylines(img, [pts], True, (0, 0, 255), 3)
+
+font = cv2.FONT_HERSHEY_DUPLEX
+cv2.putText(img, 'opencv', (450, 430), font, 1, (100, 0, 255), 2, cv2.LINE_AA)
+
+cv2.imshow('image', img)
+
+cv2.waitKey()
+cv2.destroyAllWindows()
--- OpenCV/OpenCVTut/VideoLoad.py
+++ OpenCV/OpenCVTut/VideoLoad.py
@@ -4,7 +4,7 @@ cap = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2XYZ)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     cv2.imshow('frame', frame)
     cv2.imshow('gray', gray)
     if cv2.waitKey(2) & 0xFF == ord('q'):
@@ -12,3 +12,6 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
+cv2.imwrite('pic.png', frame)
+
+cv2.imwrite('gray.png', gray)
