5a4b0b5bcc67cb7412e4a06366e3e553b5c280d6
Priyanker <priyankerrao@gmail.com>
2018-05-28 20:43:53 +0530

    Added face detection programs

--- OpenCV/OpenCVTut/facedetectionPicture.py
+++ OpenCV/OpenCVTut/facedetectionPicture.py
@@ -7,7 +7,7 @@ import cv2
 # https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
+smile_cascade = cv2.CascadeClassifier('haarcascade_smile.xml')
 # https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_eye.xml
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
@@ -23,6 +23,12 @@ for (x, y, w, h) in faces:
     eyes = eye_cascade.detectMultiScale(roi_gray)
     for (ex, ey, ew, eh) in eyes:
         cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), ((205, 220, 57)), 2)
+
+    smile = smile_cascade.detectMultiScale(roi_gray)
+    for (ex, ey, ew, eh) in smile:
+        cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), ((105, 120, 157)), 2)
+        #cv2.putText(roi_color, 'smile', (ex, ey), cv2.FONT_HERSHEY_DUPLEX, 0.7, (100, 0, 255), 2, cv2.LINE_AA)
+
 
 cv2.imshow('img', img)
 cv2.waitKey(0)
