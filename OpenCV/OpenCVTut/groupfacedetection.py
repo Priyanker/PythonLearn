6a00073951b3fd0a2ca5eda050623cb0571152ed
Priyanker <priyankerrao@gmail.com>
2018-05-29 10:11:52 +0530

    added group face detection program

--- OpenCV/OpenCVTut/groupfacedetection.py
+++ OpenCV/OpenCVTut/groupfacedetection.py
@@ -1,7 +1,7 @@
 # Haar Cascade Object Detection Face & Eye
 
 import cv2
-
+import copy
 # multiple cascades: https://github.com/Itseez/opencv/tree/master/data/haarcascades
 
 # https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml
@@ -11,26 +11,26 @@ smile_cascade = cv2.CascadeClassifier('haarcascade_smile.xml')
 # https://github.com/Itseez/opencv/blob/master/data/haarcascades/haarcascade_eye.xml
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
-img = cv2.imread('images/96.jpg')
+img = cv2.imread('images/JL/JL.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-faces = face_cascade.detectMultiScale(gray, 1.02, 4)
+faces = face_cascade.detectMultiScale(gray)
 
+resface = copy.deepcopy(img)
+count = 0
 for (x, y, w, h) in faces:
     cv2.rectangle(img, (x, y), (x+w, y+h), (66, 165, 245), 2)
     roi_gray = gray[y:y+h, x:x+w]
     roi_color = img[y:y+h, x:x+w]
+    roi_color1 = resface[y:y+h, x:x+w]
+    s1 = 's'+str(count)
+    cv2.imshow(s1, roi_color1)
+    loc = "images/JL/"
+    cv2.imwrite(loc+s1+".png", roi_color1)
+    count = count + 1
 
-    eyes = eye_cascade.detectMultiScale(roi_gray)
-    for (ex, ey, ew, eh) in eyes:
-        cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), ((205, 220, 57)), 2)
-
-    smile = smile_cascade.detectMultiScale(roi_gray)
-    for (ex, ey, ew, eh) in smile:
-        cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), ((105, 120, 157)), 2)
-        # cv2.putText(roi_color, 'smile', (ex, ey), cv2.FONT_HERSHEY_DUPLEX, 0.7, (100, 0, 255), 2, cv2.LINE_AA)
 
 cv2.imshow('img', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-cv2.imwrite('images/fdout.jpg', img)
+cv2.imwrite('images/JL/fdout.jpg', img)
